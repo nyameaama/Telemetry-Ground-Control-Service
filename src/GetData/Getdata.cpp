64a5e9cea25ad7404253a70933bb0b78f5f1447e
Nyameaama Gambrah <nyameaama@Nyameaamas-MacBook-Pro.local>
2020-05-10 12:35:02 +0000

    Improvements

--- src/GetData/Getdata.cpp
+++ src/GetData/Getdata.cpp
@@ -1,2 +1,10 @@
 #include"Getdata.h"
 
+String dataGet::fileReceive(){
+    RH_ASK driver;
+    uint8_t buf[12];
+    uint8_t buflen = sizeof(buf);
+    if (driver.recv(buf, &buflen)){ // Non-blocking
+        return String((char*)buf);
+    }    
+}
--- src/GetData/Getdata.h
+++ src/GetData/Getdata.h
@@ -6,14 +6,7 @@
 
 class dataGet {
     public:
-        String fileReceive(){
-            RH_ASK driver;
-            uint8_t buf[12];
-            uint8_t buflen = sizeof(buf);
-            if (driver.recv(buf, &buflen)){ // Non-blocking
-                return String((char*)buf);
-            }    
-        }
+        String fileReceive();
 
 };
 
